25cf46b55e94145ee3f006c99835458ecb749709
luan-teixeira <luansantos1998@hotmail.com>
2022-07-20 16:48:48 -0300

    estruturei html, comecei js, cipher e css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const cifrar = document.querySelector("#criptografar");
+
+cifrar.addEventListener("click",function(e){
+    
+    e.preventDefault();
+
+    const valordeslocamento = document.querySelector("#deslocamento").value;
+    const valortexto = document.querySelector("#texto").value;
+    const resultado = cipher.encode(valordeslocamento,valortexto);
+
+    console.log(resultado);
+
+});
+
+
+const decifrar = document.querySelector("#descriptografar");
+
+decifrar.addEventListener("click",function(e){
+
+    e.preventDefault();
+
+    const valordeslocamentotwo = document.querySelector("#deslocamento").value;
+    const valortextotwo = document.querySelector("#texto").value;
+    const resultado = ciphertwo.decode(valordeslocamentotwo,valortextotwo);
+
+    console.log(resultado);
+
+});
+
+
+
+
