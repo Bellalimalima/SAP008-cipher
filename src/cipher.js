43d3f53bf0829a008de420344f4b4867180b635d
Bellalimalima <isabellalimaelima@gmail.com>
2022-07-22 15:18:45 -0300

    Lógica decode funcionandoooo

--- src/cipher.js
+++ src/cipher.js
@@ -1,24 +1,21 @@
-const cipher = {encode,decode}
+const cipher = { encode, decode };
 
-function encode(offset,string){
-  console.log(typeof offset)
-  let converterTexto = ""
-  for(let i = 0, j= string.length; i < j; i++){
+function encode(offset, string) {
+  let converterTexto = "";
+  for (let i = 0, j = string.length; i < j; i++) {
     let numAscii = ((string.charCodeAt(i) - 65 + offset) % 26) + 65;
-    console.log(numAscii);
-    converterTexto += String.fromCharCode(numAscii)
+    converterTexto += String.fromCharCode(numAscii);
   }
   return converterTexto;
 }
 
-function decode(offset,string){
-
-    return offset + string
-
-
-
-
+function decode(offset, string) {
+  let converterTexto = "";
+  for (let i = 0, j = string.length; i < j; i++) {
+    let numAscii = ((string.charCodeAt(i) - 65 - offset) % 26) + 65;
+    converterTexto += String.fromCharCode(numAscii);
   }
-
+  return converterTexto;
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,34 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
 const cifrar = document.querySelector("#criptografar");
 
-cifrar.addEventListener("click",function(e){
-    
-    e.preventDefault();
+cifrar.addEventListener("click", function (e) {
+  e.preventDefault();
 
-    let valorDeslocamento = parseInt(document.querySelector("#deslocamento").value);
-    let valorTexto = document.querySelector("#texto").value;
-    valorTexto = valorTexto.toUpperCase();
-    let resultado = cipher.encode(valorDeslocamento,valorTexto);
-    document.getElementById("resultado").innerHTML= "Sua mensagem está pronta:" + resultado;
-    console.log(resultado);
+  let valorDeslocamento = parseInt(
+    document.querySelector("#deslocamento").value
+  );
+  let valorTexto = document.querySelector("#texto").value;
+  valorTexto = valorTexto.toUpperCase();
+  let resultado = cipher.encode(valorDeslocamento, valorTexto);
+  document.getElementById("resultado").innerHTML =
+    "Sua mensagem está pronta:" + resultado;
 
-    console.log(resultado);
-
+  console.log(resultado);
 });
-
 
 const decifrar = document.querySelector("#descriptografar");
 
-decifrar.addEventListener("click",function(e){
+decifrar.addEventListener("click", function (e) {
+  e.preventDefault();
 
-    e.preventDefault();
+  let valorDeslocamento = parseInt(
+    document.querySelector("#deslocamento").value
+  );
+  let valorTexto = document.querySelector("#texto").value;
+  let resultado = cipher.decode(valorDeslocamento, valorTexto);
+  document.getElementById("resultado").innerHTML =
+    "Sua mensagem está pronta:" + resultado;
 
-    let valorDeslocamento = document.querySelector("#deslocamento").value;
-    let valorTexto = document.querySelector("#texto").value;
-    let resultado = cipher.decode(valorDeslocamento,valorTexto);
-
-    console.log(resultado);
-
+  console.log(resultado);
 });
-
-
-
-
