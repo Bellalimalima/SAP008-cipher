47013002ac1f3ddacd7cd289f77ea2a452527250
Bellalimalima <isabellalimaelima@gmail.com>
2022-07-22 14:19:39 -0300

    Lógica de encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,13 +1,15 @@
 const cipher = {encode,decode}
 
 function encode(offset,string){
+  let converterTexto = ""
+  for(let i = 0, j= string.length; i < j; i++){
+    let numAscii = ((string.charCodeAt(i) - 65 + offset) % 26) + 65;
+    console.log(numAscii);
+    converterTexto += String.fromCharCode(numAscii)
+  }
 
 
-
-
-
-
-  return offset + string
+  return converterTexto;
 }
 
 function decode(offset,string){
