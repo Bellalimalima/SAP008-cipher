cb122167dea37bbe73243478f553dd7e1b93ae45
Bellalimalima <isabellalimaelima@gmail.com>
2022-07-28 14:12:05 -0300

    Updates

---
+++ index.js
@@ -0,0 +1,30 @@
+import cipher from "./cipher.js";
+
+const cifrar = document.querySelector("#criptografar");
+
+cifrar.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  let valorDeslocamento = parseInt(
+    document.querySelector("#deslocamento").value
+  );
+  let valorTexto = document.querySelector("#texto").value;
+  valorTexto = valorTexto.toUpperCase();
+  let resultado = cipher.encode(valorDeslocamento, valorTexto);
+  document.getElementById("resultado").innerHTML =
+    "Sua mensagem está pronta:" + resultado;
+});
+
+const decifrar = document.querySelector("#descriptografar");
+
+decifrar.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  let valorDeslocamento = parseInt(
+    document.querySelector("#deslocamento").value
+  );
+  let valorTexto = document.querySelector("#texto").value;
+  let resultado = cipher.decode(valorDeslocamento, valorTexto);
+  document.getElementById("resultado").innerHTML =
+    "Sua mensagem está pronta:" + resultado;
+});
